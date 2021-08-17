e19dce29529c37ab6ce18c54d4ff8aedd635ee4b
Levi Kohler <kohler.la01@gmail.com>
2021-08-17 15:08:50 -0700

    add hit detection for bottom border and game over alert that lets user know their score

--- scripts.js
+++ scripts.js
@@ -2,17 +2,24 @@ var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
+var counter = 0;
 
 hole.addEventListener('animationiteration', () => {
   var random = -((Math.random() * 300) + 150);
   var top = (random * 100) + 150;
   hole.style.top = random + "px";
+  counter++;
 });
 
 setInterval(function () {
   var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   if (jumping == 0) {
     character.style.top = (characterTop + 3) + "px";
+  }
+  if (characterTop > 480) {
+    alert("Game over. Score: " + counter);
+    character.style.top = 100 + "px";
+    counter = 0;
   }
 }, 10);
 
