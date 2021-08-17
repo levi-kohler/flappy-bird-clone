8b7f1e74df172481e0e6e7705bb459aacc311378
Levi Kohler <kohler.la01@gmail.com>
2021-08-17 14:48:31 -0700

    add conditional so character only moves down if not jumping

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
   var random = -((Math.random() * 300) + 150);
@@ -10,9 +11,11 @@ hole.addEventListener('animationiteration', () => {
 
 setInterval(function () {
   var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  character.style.top = (characterTop + 3) + "px";
+  if (jumping == 0) {
+    character.style.top = (characterTop + 3) + "px";
+  }
 }, 10);
 
 function jump() {
-
+  jumping = 1;
 }
